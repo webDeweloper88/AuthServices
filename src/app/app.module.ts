1916b9b5392069f7ab19bb5864f86ea78387f9e0
Your Name <you@example.com>
2024-08-12 03:59:50 +0500

    modified:   docker-compose.yml 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.module.ts 	modified:   src/config/configuration.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import configuration from '@config/configuration';
 
-
-
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal:true,
-    load:[configuration]
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration], // Загружает вашу конфигурацию
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+             return{
+          dialect: 'postgres',
+          host: configService.get<string>('db_host'),
+          port: configService.get<number>('db_port'),
+          username: configService.get<string>('db_user'),
+          password: configService.get<string>('db_password'),
+          database: configService.get<string>('db_name'),
+          autoLoadModels: true,
+          synchronize: true,
+        }
+       
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,24 +1,24 @@
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
-    database: {
-      host: process.env.DATABASE_HOST || 'localhost', // Добавьте значение по умолчанию
-      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      name: process.env.POSTGRES_DB,
-    },
-    jwt: {
+   
+      db_host: process.env.DATABASE_HOST || 'localhost', // Добавьте значение по умолчанию
+      db_port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      db_user: process.env.POSTGRES_USER,
+      db_password: process.env.POSTGRES_PASSWORD,
+      db_name: process.env.POSTGRES_DB,
+    
+    
       secret: process.env.SECRET_JWT || 'defaultSecret',
       expiresIn: process.env.EXPIRE_JWT || '2h',
-    },
-    mailService: {
-      host: process.env.MAIL_HOST,
-      port: parseInt(process.env.MAIL_PORT, 10) || 465,
-      user: process.env.MAIL_USER,
-      password: process.env.MAIL_PASSWORD,
+    
+    
+      mailHost: process.env.MAIL_HOST,
+      mailPort: parseInt(process.env.MAIL_PORT, 10) || 465,
+      mailUser: process.env.MAIL_USER,
+      mailPassword: process.env.MAIL_PASSWORD,
       from: process.env.MAIL_FROM,
-    },
-    appUrl: process.env.APP_URL || 'http://localhost:3000',
-    appUrlFront: process.env.APP_URL_FRONT || 'http://localhost:5173',
+    
+      mailUrl: process.env.APP_URL || 'http://localhost:3000',
+      mailUrlFront: process.env.APP_URL_FRONT || 'http://localhost:5173',
   });
   
